f74b6108a69ba6047ea0d38bdb1ca2fef34785f5
skrnpc <kirnapcsinan@gmail.com>
2024-02-16 16:52:46 +0300

    svg icon eklemeleri yapıldı

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,26 +1,35 @@
+import { ReactComponent as CourseSvg } from "../svgImage/course.svg";
+import { ReactComponent as StuSvg } from "../svgImage/student.svg";
+import { ReactComponent as PaySvg } from "../svgImage/payment.svg";
+import { ReactComponent as UsersSvg } from "../svgImage/users.svg";
+
 function HomeScreen() {
   return (
     <>
       <div className="p-10 flex ">
-        <div className="p-5 w-64 h-32 bg-orange-500 bg-opacity-50 text-white relative mr-6">
+        <div className="p-3 w-64 h-32 bg-orange-500 bg-opacity-50 text-white relative mr-6">
+          <StuSvg className="w-7 h-7 mb-1" />
           <p className="text-lg">Students</p>
           <p className="absolute bottom-0 right-0 p-4 mr-4 text-black font-bold text-xl">
             243
           </p>
         </div>
-        <div className="p-5 w-64 h-32 bg-orange-500 bg-opacity-50 text-white relative mr-6">
+        <div className="p-3 w-64 h-32 bg-orange-500 bg-opacity-50 text-white relative mr-6">
+          <CourseSvg className=" w-7 h-7 mb-1" />
           <p className="text-lg">Course</p>
           <p className="absolute bottom-0 right-0 p-4 mr-4 text-black font-bold text-xl">
             13
           </p>
         </div>
-        <div className="p-5 w-64 h-32 bg-orange-500 bg-opacity-50 text-white relative mr-6">
+        <div className="p-3 w-64 h-32 bg-orange-500 bg-opacity-50 text-white relative mr-6">
+          <PaySvg className="w-7 h-7 mb-1 " />
           <p className="text-lg">Payments</p>
           <p className="absolute bottom-0 right-0 p-4 mr-4 text-black font-bold text-xl">
             500,000₺
           </p>
         </div>
-        <div className="p-5 w-64 h-32 bg-orange-500 bg-opacity-50 text-white relative mr-6">
+        <div className="p-3 w-64 h-32 bg-orange-500 bg-opacity-50 text-white relative mr-6">
+          <UsersSvg className="w-7 h-7 mb-1" />
           <p className="text-lg">Users</p>
           <p className="absolute bottom-0 right-0 p-4 mr-4 text-black font-bold text-xl">
             3
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,24 +1,49 @@
+import { ReactComponent as HomeSvg } from "../svgImage/home.svg";
+import { ReactComponent as CourseSvg } from "../svgImage/course.svg";
+import { ReactComponent as StuSvg } from "../svgImage/student.svg";
+import { ReactComponent as PaySvg } from "../svgImage/payment.svg";
+import { ReactComponent as RepSvg } from "../svgImage/report.svg";
+import { ReactComponent as SetSvg } from "../svgImage/settings.svg";
+
 function Menu() {
   return (
     <>
       <div className="flex flex-col items-center justify-center mt-24">
-        <button className=" bg-red-300 hover:bg-red-500 text-white font-bold py-2 px-4 rounded mt-4 w-32">
-          Home
+        <button className="bg-red-300 hover:bg-red-500 text-white font-bold py-2 px-4 rounded mt-4 w-32 flex">
+          <div className="flex items-center">
+            <HomeSvg className="w-6 h-6 mr-2 " />
+            <p className="m-0">Home</p>
+          </div>
         </button>
-        <button className=" bg-red-300 hover:bg-red-500 text-white font-bold py-2 px-4 rounded mt-4 w-32">
-          Course
+        <button className="bg-red-300 hover:bg-red-500 text-white font-bold py-2 px-4 rounded mt-4 w-32 flex ">
+          <div className="flex items-start">
+            <CourseSvg className="w-6 h-6 mr-2 " />
+            <p className="m-0">Course</p>
+          </div>
         </button>
-        <button className=" bg-red-300 hover:bg-red-500 text-white font-bold py-2 px-4 rounded mt-4 w-32">
-          Student
+        <button className="bg-red-300 hover:bg-red-500 text-white font-bold py-2 px-4 rounded mt-4 w-32 flex ">
+          <div className="flex items-start">
+            <StuSvg className="w-6 h-6 mr-2" />
+            <p className="m-0">Students</p>
+          </div>
         </button>
-        <button className=" bg-red-300 hover:bg-red-500 text-white font-bold py-2 px-4 rounded mt-4 w-32">
-          Payment
+        <button className="bg-red-300 hover:bg-red-500 text-white font-bold py-2 px-4 rounded mt-4 w-32 flex ">
+          <div className="flex items-center">
+            <PaySvg className="w-6 h-6 mr-2" />
+            <p className="m-0">Payment</p>
+          </div>
         </button>
-        <button className=" bg-red-300 hover:bg-red-500 text-white font-bold py-2 px-4 rounded mt-4 w-32">
-          Report
+        <button className="bg-red-300 hover:bg-red-500 text-white font-bold py-2 px-4 rounded mt-4 w-32 flex ">
+          <div className="flex items-center">
+            <RepSvg className="w-6 h-6 mr-2" />
+            <p className="m-0">Report</p>
+          </div>
         </button>
-        <button className=" bg-red-300 hover:bg-red-500 text-white font-bold py-2 px-4 rounded mt-4 w-32">
-          Settings
+        <button className="bg-red-300 hover:bg-red-500 text-white font-bold py-2 px-4 rounded mt-4 w-32 flex ">
+          <div className="flex items-center">
+            <SetSvg className="w-6 h-6 mr-2" />
+            <p className="m-0">Settings</p>
+          </div>
         </button>
       </div>
     </>
