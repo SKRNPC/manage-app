b5874ec6ee0cee60370a906cbb02ae5796d2e2fd
skrnpc <kirnapcsinan@gmail.com>
2024-03-15 14:24:15 +0300

    rastgele token

--- src/redux/features/authSlice.js
+++ src/redux/features/authSlice.js
@@ -3,11 +3,21 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 export const login = createAsyncThunk(
   "auth/login",
   async (_, { fulfillWithValue }) => {
-    const token = "fakeToken123";
+    const token = generateRandomToken(20); // 20 karakter uzunluğunda rastgele bir token üret
     localStorage.setItem("token", token);
     return fulfillWithValue({ user: "defaultUser", token });
   }
 );
+function generateRandomToken(length) {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 const initialState = {
   user: null,
